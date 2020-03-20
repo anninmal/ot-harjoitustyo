62615a6d1841c5126f30486ba63b5b3f61afac68
Annina Malmsten <annina.malmsten@helsinki.fi>
2020-03-20 21:25:50 +0200

    viikko2 laskarit osat 2-4 tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void alksusaldoOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenToimiiOikein() {
+        kortti.lataaRahaa(5);
+        assertEquals("saldo: 0.15", kortti.toString());
+    }
+    
+    @Test
+    public void rahaVaheneeOikein() {
+        kortti.otaRahaa(6);
+        assertEquals("saldo: 0.4", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaEiRiita() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaTrueJosTarpeeksiRahaa () {
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void palauttaaFalseJosEiTarpeeksiRahaa () {
+        assertFalse(kortti.otaRahaa(15));
+    }
+    
+    @Test
+    public void palauttaaSaldon() {
+        assertEquals(10, kortti.saldo());
+    }
 }
